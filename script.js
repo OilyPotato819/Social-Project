b7597b3c108f3b9caafaf7fffd75473c730f82f7
OilyPotato819 <o.schneider@share.epsb.ca>
2022-01-10 22:48:52 -0700

    started to add rotating mirrors

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function startGame() {
   if (screen === "title") {
     if (distance < 30) {
       screen = "level1";
-      level1Setup();
+      puzzle1Setup();
       document.body.style.cursor = "default";
     }
   }
@@ -55,7 +55,7 @@ function startGame() {
 
 function mousemoveHandler(event) {
   let cnvRect = cnv.getBoundingClientRect();
-  console.log("x: " + (event.x - cnvRect.x), "y: " + (event.y - cnvRect.y));
+  // console.log("x: " + (event.x - cnvRect.x), "y: " + (event.y - cnvRect.y));
   if (screen === "title") {
     let run = Math.abs(event.x - cnvRect.x - 500);
     let rise = Math.abs(event.y - cnvRect.y - 300);
@@ -110,27 +110,28 @@ function keyupHandler(event) {
 // CREATE LEVEL OBJECTS
 
 // Platform function
-function newBlock(x, y, w, h, action, color) {
+function newBlock(x, y, w, h, a, r, c) {
   return {
     x: x,
     y: y,
     w: w,
     h: h,
-    action: action,
-    color: color,
+    a: a,
+    r: r,
+    c: c,
   }
 }
 
 // Create objects
 let platform1 = newBlock(0, 0, 50, 10);
 let platform2 = newBlock(0, 0, 50, 10);
-let lWall1 = newBlock(0, 0, 10, 50);
-let lWall2 = newBlock(0, 0, 10, 50);
-let pWall1 = newBlock(0, 0, 1, 0);
-let pWall2 = newBlock(0, 0, 1, 0);
-let activateHole = newBlock(0, 0, 45, 35, "hole");
+let wallL1 = newBlock(0, 0, 10, 50);
+let wallL2 = newBlock(0, 0, 10, 50);
+let wallC1 = newBlock(0, 0, 1, 0);
+let wallC2 = newBlock(0, 0, 1, 0);
+let entrance = newBlock(0, 0, 45, 35, "hole");
 let hole = newBlock(0, 474, 100, 0);
-let mirror = newBlock(0, 0, 50, 10);
+let mirror = newBlock(0, 0, 50, 10, "mirror", 90);
 
 // MAIN PROGRAM LOOP
 requestAnimationFrame(loop);
@@ -171,15 +172,23 @@ function loop() {
     ctx.fillRect(platform1.x, platform1.y, platform1.w, platform1.h);
     ctx.fillRect(platform2.x, platform2.y, platform2.w, platform2.h);
 
-
-
     // Walls
-    ctx.fillRect(lWall1.x, lWall1.y, lWall1.w, lWall1.h);
-    ctx.fillRect(lWall2.x, lWall2.y, lWall2.w, lWall2.h);
-    ctx.fillRect(activateHole.x, activateHole.y, activateHole.w, activateHole.h);
-    ctx.fillStyle = hole.color;
-    ctx.fillRect(pWall1.x, pWall1.y, pWall1.w, pWall1.h);
-    ctx.fillRect(pWall2.x, pWall2.y, pWall2.w, pWall2.h);
+    ctx.fillRect(wallL1.x, wallL1.y, wallL1.w, wallL1.h);
+    ctx.fillRect(wallL2.x, wallL2.y, wallL2.w, wallL2.h);
+    ctx.fillRect(entrance.x, entrance.y, entrance.w, entrance.h);
+    ctx.fillStyle = hole.c;
+    ctx.fillRect(wallC1.x, wallC1.y, wallC1.w, wallC1.h);
+    ctx.fillRect(wallC2.x, wallC2.y, wallC2.w, wallC2.h);
+
+    // Mirror
+    let mcx = mirror.x + 0.5 * mirror.w;
+    let mcy = mirror.y + 0.5 * mirror.h;
+    ctx.save();
+    ctx.translate(mcx, mcy);
+    ctx.rotate(mirror.r * Math.PI / 180);
+    ctx.translate(-mcx, -mcy);
+    ctx.fillRect(mirror.x, mirror.y, mirror.w, mirror.h);
+    ctx.restore();
 
     // Hole
     ctx.fillRect(hole.x, hole.y, hole.w, hole.h);
@@ -214,14 +223,26 @@ function loop() {
         function wallCollide(aWall) {
           if (laser.xLine > aWall.x && laser.xLine < aWall.x + aWall.w && laser.y > aWall.y && laser.y < aWall.y + aWall.h) {
             laser.collide = true;
-            if (aWall.action === "hole") {
+            if (aWall.a === "hole") {
               openHole = true;
             }
           }
         }
-        wallCollide(lWall1);
-        wallCollide(lWall2);
-        wallCollide(activateHole);
+        wallCollide(wallL1);
+        wallCollide(wallL2);
+        wallCollide(entrance);
+
+        function mirrorCollide(aMirror) {
+          if (laser.xLine > aMirror.x && laser.xLine < aMirror.x + aMirror.w && laser.y > aMirror.y && laser.y < aMirror.y + aMirror.h) {
+            console.log("yee")
+            // mirror.x = 400;
+            // mirror.y = 444;
+            // 421
+            // 425
+          }
+        }
+        mirrorCollide(mirror);
+
         if (laser.xLine < -1 || laser.xLine > cnv.width + 1) {
           laser.collide = true;
         }
@@ -253,14 +274,14 @@ function loop() {
 
     if (openHole) {
       if (hole.h < cnv.height) {
-        hole.h += 2;
+        hole.h += 4;
       }
-      pWall1.x = hole.x;
-      pWall1.y = hole.y;
-      pWall1.h = hole.h;
-      pWall2.x = hole.x + 100;
-      pWall2.y = hole.y;
-      pWall2.h = hole.h;
+      wallC1.x = hole.x;
+      wallC1.y = hole.y;
+      wallC1.h = hole.h;
+      wallC2.x = hole.x + 100;
+      wallC2.y = hole.y;
+      wallC2.h = hole.h;
     }
 
     // CHARACTER
@@ -287,8 +308,8 @@ function loop() {
           }
         }
       }
-      wallCollide(pWall1);
-      wallCollide(pWall2);
+      wallCollide(wallC1);
+      wallCollide(wallC2);
     }
 
     // prevent character from going off screen
@@ -303,13 +324,13 @@ function loop() {
     if (laser.shoot != true) {
       // jump
       char.y += char.gravity;
-      if (char.gravity <= 10) {
-        char.gravity += 0.5;
+      if (char.gravity < 8.5) {
+        char.gravity += 0.45;
       }
       if (upIsPressed && char.standing) {
         char.standing = false;
-        char.y -= 10;
-        char.gravity = -10;
+        char.y -= 8.5;
+        char.gravity = -8.5;
       }
 
       // check if standing on something
@@ -352,67 +373,57 @@ function loop() {
 function level1Setup() {
   if (screen = "level1") {
     background = document.getElementById("level1");
-    lWall1.x = -50;
-    lWall1.y = -50;
-    lWall2.x = -50;
-    lWall2.y = -50;
-    activateHole.x = 549;
-    activateHole.y = 386;
-    activateHole.w = 45.1;
-    activateHole.h = 50;
-    platform1.x = -50;
-    platform1.y = -50;
-    platform2.x = -50;
-    platform2.y = -50;
+    hideAll();
+    entrance.x = 549;
+    entrance.y = 386;
+    entrance.w = 45.1;
+    entrance.h = 50;
     hole.x = 920;
-    hole.color = "#cea61b"
+    hole.c = "#ffeb3b"
   }
 }
 
 function level2Setup() {
   if (screen = "level2") {
     // background = document.getElementbyId("level2");
+    // hideAll();
   }
 }
 
 function level3Setup() {
   if (screen = "level3") {
     // background = document.getElementbyId("level3");
-
+    // hideAll();
   }
 }
 
 function level4Setup() {
   if (screen = "level4") {
     // background = document.getElementbyId("level4");
-    // activateHole.x = 549;
-    // activateHole.y = 386;
-    // activateHole.w = 45.1;
-    // activateHole.h = 50;
+    // hideAll();
+    // entrance.x = 549;
+    // entrance.y = 386;
+    // entrance.w = 45.1;
+    // entrance.h = 50;
   }
 }
 
 function level5Setup() {
   if (screen = "level5") {
     // background = document.getElementbyId("level5");
-
+    // hideAll();
   }
 }
 
 function puzzle1Setup() {
-  console.log("yee")
   if (screen = "puzzle1") {
     background = document.getElementById("puzzle");
     openHole = false;
-    hole.x = -50;
-    hole.y = -50;
-    hole.w = -50;
-    hole.h = -50;
-    activateHole.x = -50;
-    activateHole.y = -50;
-    activateHole.w = -50;
-    activateHole.h = -50;
+    entrance.x = -100;
+    hole.x = -100;
     char.y = 0 - char.h;
+    mirror.x = 400;
+    mirror.y = 444;
   }
 }
 
@@ -438,6 +449,16 @@ function puzzle5Setup() {
   if (screen = "puzzle5") {
 
   }
+}
+
+function hideAll() {
+  wallL1.x = -100;
+  wallL2.x = -100;
+  wallC1.x = -100
+  wallC2.x = -100;
+  mirror.x = -100;
+  platform1.x = -100;
+  platform2.x = -100;
 }
 
 // LEVEL 1: Route de la Soie
